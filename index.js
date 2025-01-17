c9fa8f7c895b6a316d15bfceb28c4eb9803684b7
Dillan04 <dillanhammond7@gmail.com>
2025-01-17 17:30:48 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const { CreateChannel, SubscribeMessage } = require("./utils");
+const userRoutes = require("./api/user");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const print = console.log;
+const port = process.env.PORT || 8001
+dotenv.config(); // Load environment variables from .env file
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(express.static(__dirname + "/public"));
+
+async function startApp() {
+  try {
+    await mongoose.connect(process.env.DB_URI);
+    print("Connection sauce");
+
+    const channel = await CreateChannel();
+
+    await userRoutes(app, channel);
+
+    app.listen(port, () => {
+      console.log(`Customer is Listening to Port ${port}`);
+    });
+  } catch (err) {
+    console.log("Failed to start app:", err);
+  }
+}
+
+startApp();
