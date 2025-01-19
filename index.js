63ea9bc0ab721f9e06fcf4eac24e27352cdbb399
Dillan04 <dillanhammond7@gmail.com>
2025-01-19 21:25:03 +0100

    6th commit

--- index.js
+++ index.js
@@ -5,12 +5,35 @@ const userRoutes = require("./api/user");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const print = console.log;
-const port = process.env.PORT || 8001
+const port = process.env.PORT || 8001;
 dotenv.config(); // Load environment variables from .env file
 
 const app = express();
+
+// CORS configuration to handle preflight and allow only specific origins
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Define allowed origins
+    const allowedOrigins = [
+      'https://multivendorapp-frontend.onrender.com', // Production Frontend URL
+      'http://localhost:3000' // Localhost URL (if running locally)
+    ];
+
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+};
+
+// Use CORS middleware with specific options
+app.use(cors(corsOptions));
+
 app.use(express.json());
-app.use(cors());
 app.use(express.static(__dirname + "/public"));
 
 async function startApp() {
